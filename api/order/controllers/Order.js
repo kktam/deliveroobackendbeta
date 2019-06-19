ec05a051c89fcae593d45706f8866bfac3c65b24
Ka Tam <nkltam2003@gmail.com>
2019-06-19 09:59:28 -0700

    Upgrade order to 3.0.0_beta.5
    got bug
    
    [2019-06-19T16:57:42.806Z] error Error: SQLITE_ERROR: table orders has no column named user

--- api/order/controllers/Order.js
+++ api/order/controllers/Order.js
@@ -5,4 +5,37 @@
  * to customize this controller
  */
 
-module.exports = {};
+const stripe = require("stripe")("sk_test_m1jtku3UIo5rKCMch9M1gUt500NoYdVPsH");
+
+module.exports = {
+
+    /**
+   * Create a/an order record.
+   *
+   * @return {Object}
+   */
+
+  create: async ctx => {
+    const { address, amount, dishes, token, city } = ctx.request.body;
+
+    const charge = await stripe.charges.create({
+      // Transform cents to dollars.
+      amount: amount * 100,
+      currency: "usd",
+      description: `Order ${new Date()} by ${ctx.state.user._id}`,
+      source: token
+    });
+
+    // Register the order in the database
+    const order = await strapi.services.order.add({
+      user: ctx.state.user._id,
+      address,
+      amount,
+      dishes,
+      city
+    });
+
+    return order;
+  },
+
+};
--- api/order/services/Order.js
+++ api/order/services/Order.js
@@ -5,4 +5,31 @@
  * to customize this service
  */
 
-module.exports = {};
+// Public dependencies.
+const _ = require('lodash');
+
+// Strapi utilities.
+const utils = require('strapi-hook-bookshelf/lib/utils/');
+const { convertRestQueryParams, buildQuery } = require('strapi-utils'); 
+
+module.exports = {
+
+ /**
+   * Promise to add a/an order.
+   *
+   * @return {Promise}
+   */
+
+  add: async (values) => {
+    // Extract values related to relational data.
+    const relations = _.pick(values, Order.associations.map(ast => ast.alias));
+    const data = _.omit(values, Order.associations.map(ast => ast.alias));
+
+    // Create entry with no-relational data.
+    const entry = await Order.forge(data).save();
+
+    // Create relational data and return the entry.
+    return Order.updateRelations({ id: entry.id , values: relations });
+  },
+
+};
