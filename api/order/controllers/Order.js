f7ec7cf61b095088a2b5ed8e2b7e159d008c433c
Ka Tam <nkltam2003@gmail.com>
2019-06-19 16:26:00 -0700

    Orders with users
    recorded correct users

--- api/order/controllers/Order.js
+++ api/order/controllers/Order.js
@@ -18,22 +18,28 @@ module.exports = {
   create: async ctx => {
     const { address, amount, dishes, token, city } = ctx.request.body;
 
+    //console.log("ctx.state: %j", ctx.state);  
+
     const charge = await stripe.charges.create({
       // Transform cents to dollars.
       amount: amount * 100,
       currency: "usd",
-      description: `Order ${new Date()} by ${ctx.state.user._id}`,
+      description: `Order ${new Date()} by ${ctx.state.user.id}`,
       source: token
     });
 
+    //console.log("stripe has been charged"); 
+
     // Register the order in the database
     const order = await strapi.services.order.add({
-      user: ctx.state.user._id,
+      user: ctx.state.user.id,
       address,
       amount,
       dishes,
       city
     });
+
+    //console.log("called strapi add order record");    
 
     return order;
   },
